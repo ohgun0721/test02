a1f5d9a73b48c67c229ca2e6d8d2b1d8ed8967c4
ohgun <ohgun00721@naver.com>
2024-04-16 12:57:42 +0900

    third commit

---
+++ Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace project_2
+{
+    public partial class Form1 : Form
+    {
+        static int zzz = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            textBox1.Text = zzz.ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1_Tick(sender, e);
+            zzz += 1; 
+        }
+    }
+}
